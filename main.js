fe8f8588f0f56d4534cb45d0e6201d8cfe5d156d
Yanis DANDO <yanis.dando@gmail.com>
2018-10-19 14:54:21 +0200

    correcting dropdown list system

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ var app = new Vue({
   },
   created: function() 
   {
+    vm = this;
     $.getJSON( "data.json", function( json ) {
-      this.users = json;
-      console.log( "my users: " + JSON.stringify(this.users));
+      vm.users = json;
+      console.log( "my users: " + JSON.stringify(vm.users));
       
     })
   },
@@ -17,28 +18,6 @@ var app = new Vue({
 
   },
   computed: {
-    finalUsers: function() { console.log("COMPUTED: "+this.users);
-      return null;
-    }
+
   },
 })
-
-/* var sel = document.getElementById('comptepanel');
-var userSelected = sel.value;
-userSelected = userSelected.substring(0, userSelected.indexOf('('));
-console.log(userSelected); */
-
-/* 
-new Vue({
-  el: '#compte',
-  data () {
-    return {
-      info: null
-    }
-  },
-  mounted () {
-    axios
-      .get('https://api.github.com/users/')
-      .then(response => (this.info = response))
-  }
-}) */
