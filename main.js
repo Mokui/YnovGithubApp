0f9657cae129e63500689c62bf0dbeeb1d8ad14a
Yanis DANDO <yanis.dando@gmail.com>
2018-10-25 14:28:16 +0200

    new datepicker and starting parsing axios data

--- main.js
+++ main.js
@@ -4,29 +4,37 @@ var app = new Vue({
     message: 'Hello Vue!',
     users: [],
     userSelected: '',
-    info: 'Mokui',
-    
+    infos: 'Please select something',
+    commitsFound: '', 
   },
-  created: function() 
+  created: function()
   {
     vm = this;
     $.getJSON( "data.json", function( json ) {
       vm.users = json;
-      console.log("my users: " + JSON.stringify(vm.users));
+      //console.log("my users: " + JSON.stringify(vm.users));
       
     })
   },
   methods: {
-    getSelectedUser : function(){
+    getSelectedUser : function() {
       vm = this;
-      console.log("User: "+vm.userSelected);
+      //console.log("User: "+vm.userSelected);
       
       axios
-        .get('https://api.github.com/repos/'+vm.userSelected+'/github-ynov-vue/commits')
-        .then(response => (vm.info = response))
+        .get(`https://api.github.com/repos/${vm.userSelected}/github-ynov-vue/commits`)
+        .then(response => (vm.infos = response.data));
+    },
+    getSelectedProject : function (repoName) {
+      vm = this;
+      console.log("Repo: "+ repoName);
+      
+      axios
+        .get(`https://api.github.com/repos/${vm.userSelected}/${repoName}/commits`)
+        .then(response => (vm.infos = response.data));
     }
   },
   computed: {
-
-  },
+    
+  }
 })
