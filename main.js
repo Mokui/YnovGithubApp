11f9fb088f4493b49604e7f10f550c72976fe55b
Yanis DANDO <yanis.dando@gmail.com>
2018-10-25 15:11:17 +0200

    hinting potential projectname and names

--- main.js
+++ main.js
@@ -3,34 +3,35 @@ var app = new Vue({
   data: {
     message: 'Hello Vue!',
     users: [],
-    userSelected: '',
-    infos: 'Please select something',
-    commitsFound: '', 
+    userSelected: 'Mokui',
+    infos: [],
+    commitsFound: '',
+    projectName: 'github-ynov-vue' ,
   },
   created: function()
   {
     vm = this;
     $.getJSON( "data.json", function( json ) {
       vm.users = json;
-      //console.log("my users: " + JSON.stringify(vm.users));
+      // console.log("my users: " + JSON.stringify(vm.users));
       
-    })
+    });
   },
   methods: {
     getSelectedUser : function() {
       vm = this;
-      //console.log("User: "+vm.userSelected);
+      // console.log("User: "+vm.userSelected);
       
       axios
         .get(`https://api.github.com/repos/${vm.userSelected}/github-ynov-vue/commits`)
         .then(response => (vm.infos = response.data));
     },
-    getSelectedProject : function (repoName) {
+    getSelectedProject : function () {
       vm = this;
-      console.log("Repo: "+ repoName);
+      // console.log("Repo: "+ vm.projectName);
       
       axios
-        .get(`https://api.github.com/repos/${vm.userSelected}/${repoName}/commits`)
+        .get(`https://api.github.com/repos/${vm.userSelected}/${vm.projectName}/commits`)
         .then(response => (vm.infos = response.data));
     }
   },
